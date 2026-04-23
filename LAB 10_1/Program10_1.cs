46cc605a265784472f39e151668ee927a841459f
VLRP4ND4 <valeragogolev130505@mail.ru>
2026-04-23 09:37:14 +0900

    добавлены два проекта из лаб 10, написан код чуть чуть и добавлена библиотека

---
+++ LAB 10_1/Program10_1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Lab10_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> actors = new List<string>
+            {
+                "Харрисон Форд",
+                "Эмма Уотсон",
+                "Иэн Маккеллен"
+            };
+
+            List<MovieInfo> movies = new List<MovieInfo>
+            {
+                new MovieInfo
+                {
+                    Title = "Индиана Джонс",
+                    Description = "Приключенческий фильм о поисках древних артефактов."
+                },
+                new MovieInfo
+                {
+                    Title = "Сумерки",
+                    Description = "Фэнтезийная история о любви девушки и вампира."
+                },
+                new MovieInfo
+                {
+                    Title = "Гарри Поттер",
+                    Description = "Фильм о юном волшебнике и его друзьях."
+                }
+            };
+
+            MovieDatabaseJson data = new MovieDatabaseJson
+            {
+                Actors = actors,
+                Movies = movies
+            };
+
+            string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\moviesData.json");
+            filePath = Path.GetFullPath(filePath);
+            File.WriteAllText(filePath, json);
+
+            Console.WriteLine("JSON-файл успешно создан.");
+            Console.WriteLine("Файл сохранён: " + Path.GetFullPath(filePath));
+            Console.WriteLine(json);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class MovieInfo
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class MovieDatabaseJson
+    {
+        public List<string> Actors { get; set; }
+        public List<MovieInfo> Movies { get; set; }
+    }
+}
---
+++ LAB 10_2/Program10_2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace LAB10_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                @"..\..\..\LAB 10_1\movies_data.json"
+            );
+
+            filePath = Path.GetFullPath(filePath);
+
+            Console.WriteLine("Поиск файла: " + filePath);
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("Файл не найден.");
+                Console.ReadKey();
+                return;
+            }
+
+            string json = File.ReadAllText(filePath);
+            MovieDatabaseJson data = JsonConvert.DeserializeObject<MovieDatabaseJson>(json);
+
+            Console.WriteLine("Актеры:");
+            foreach (string actor in data.Actors)
+            {
+                Console.WriteLine(actor);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Фильмы:");
+            foreach (MovieInfo movie in data.Movies)
+            {
+                Console.WriteLine(movie.Title + " - " + movie.Description);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class MovieInfo
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class MovieDatabaseJson
+    {
+        public List<string> Actors { get; set; }
+        public List<MovieInfo> Movies { get; set; }
+    }
+}
